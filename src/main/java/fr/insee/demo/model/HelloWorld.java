507c98b5f2e5246412216cff7534dd4eedcf164b
bwerquin <benoit.werquin@insee.fr>
2017-12-01 14:08:50 +0100

    Init with model class

---
+++ src/main/java/fr/insee/demo/model/HelloWorld.java
@@ -0,0 +1,22 @@
+package fr.insee.demo.model;
+
+public class HelloWorld {
+
+	private String message;
+	
+	public HelloWorld(String message){
+		
+		this.message=message;
+		
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+}
